33070dd9af9801eff594d4af6998eb9ccc3da6cc
clever-junior <tk_clever.junior@outlook.com>
2023-02-26 13:48:03 -0400

    feat: add requisite 1

--- tests/product/test_product.py
+++ tests/product/test_product.py
@@ -1,5 +1,18 @@
-# from inventory_report.inventory.product import Product
+import pytest
+
+from inventory_report.inventory.product import Product
 
 
+@pytest.mark.xfail
 def test_cria_produto():
-    pass  # Seu teste deve ser escrito aqui
+    product = Product(
+        id,
+        nome_do_produto="",
+        nome_da_empresa="",
+        data_de_fabricacao="",
+        data_de_validade="",
+        numero_de_serie="",
+        instrucoes_de_armazenamento="",
+    )
+
+    assert repr(product)
