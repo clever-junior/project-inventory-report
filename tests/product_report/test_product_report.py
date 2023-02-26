42f9d30e316d43850fa0f9b504a73a89214e0401
clever-junior <tk_clever.junior@outlook.com>
2023-02-26 15:58:32 -0400

    feat: add requisite 8

--- tests/product_report/test_product_report.py
+++ tests/product_report/test_product_report.py
@@ -1,5 +1,31 @@
-# from inventory_report.inventory.product import Product
+import pytest
+
+from inventory_report.inventory.product import Product
 
 
+@pytest.mark.xfail
 def test_relatorio_produto():
-    pass  # Seu teste deve ser escrito aqui
+    nome_da_empresa = "Cafes Nature"
+    nome_do_produto = "Cafe"
+    data_de_fabricacao = "2020-07-04"
+    data_de_validade = "2023-02-09"
+    numero_de_serie = "FR48"
+    instrucoes_de_armazenamento = "instrucao"
+
+    produto = Product(
+        id,
+        nome_da_empresa,
+        nome_do_produto,
+        data_de_fabricacao,
+        data_de_validade,
+        numero_de_serie,
+        instrucoes_de_armazenamento,
+    )
+
+    assert repr(produto) == (
+        f"O produto {nome_do_produto}"
+        f" fabricado em {data_de_fabricacao}"
+        f" por {nome_da_empresa} com validade"
+        f" até {data_de_validade}"
+        f" precisa ser armazenado {instrucoes_de_armazenamento}."
+    )
