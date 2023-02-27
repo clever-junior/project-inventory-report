543bc1b44d7efa8b5c372e34ec9391f0651914c5
clever-junior <tk_clever.junior@outlook.com>
2023-02-27 00:43:39 -0400

    feat: add requisite 11

--- inventory_report/main.py
+++ inventory_report/main.py
@@ -1,2 +1,32 @@
+import sys
+
+from inventory_report.importer.csv_importer import CsvImporter
+from inventory_report.importer.json_importer import JsonImporter
+from inventory_report.importer.xml_importer import XmlImporter
+from inventory_report.inventory.inventory_refactor import InventoryRefactor
+
+
 def main():
-    pass
+    try:
+        _method, file_path, report_type = sys.argv
+        importer = get_importer(file_path)
+        return sys.stdout.write(
+            InventoryRefactor(importer).import_data(file_path, report_type))
+    except ValueError:
+        return sys.stderr.write("Verifique os argumentos\n")
+
+
+def get_importer(caminho_para_o_arquivo: str):
+    extensao_do_arquivo = caminho_para_o_arquivo.rsplit(".", 1)[-1]
+
+    match extensao_do_arquivo:
+        case "csv":
+            return CsvImporter
+        case "json":
+            return JsonImporter
+        case "xml":
+            return XmlImporter
+
+
+if __name__ == "__main__":
+    main()
