43d48e8ef574e7e37537cb38e676fb5c53497cdd
clever-junior <tk_clever.junior@outlook.com>
2023-02-26 23:17:46 -0400

    feat: add requisite 9

--- tests/report_decorator/test_report_decorator.py
+++ tests/report_decorator/test_report_decorator.py
@@ -1,5 +1,24 @@
-# from inventory_report.reports.colored_report import ColoredReport
+from inventory_report.reports.colored_report import ColoredReport
+from inventory_report.reports.simple_report import SimpleReport
+
+lista_de_produtos = [
+    {
+        "id": 1,
+        "nome_do_produto": "Cafe",
+        "nome_da_empresa": "Cafes Nature",
+        "data_de_fabricacao": "2020-07-04",
+        "data_de_validade": "2023-02-09",
+        "numero_de_serie": "FR48",
+        "instrucoes_de_armazenamento": "instrucao"
+    }
+]
 
 
 def test_decorar_relatorio():
-    pass  # Seu teste deve ser escrito aqui
+    relatorio_colorido = ColoredReport(SimpleReport)
+
+    relatorio = relatorio_colorido.generate(lista_de_produtos)
+
+    assert '\033[32m' in relatorio
+    assert '\033[36m' in relatorio
+    assert '\033[31m' in relatorio
